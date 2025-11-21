4bb1ff98f3009456900c07fe1470c858d1a2eb17
Mikhail Latypov <hodkonem1984@gmail.com>
2025-11-21 13:39:57 +0300

    feat: реализован минимальный функционал Hibernate (сущность User + Session API)

--- src/main/java/ru/hodkonem/HibernateRunner.java
+++ src/main/java/ru/hodkonem/HibernateRunner.java
@@ -1,4 +1,30 @@
 package ru.hodkonem;
 
-public class HubernateRunner {
+import org.hibernate.cfg.Configuration;
+import ru.hodkonem.entity.User;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public class HibernateRunner {
+    static void main() throws SQLException {
+        var configuration = new Configuration();
+        configuration.addAnnotatedClass(User.class);
+        configuration.configure();
+        try (var sessionFactory = configuration.buildSessionFactory();
+             var session = sessionFactory.openSession()) {
+            session.beginTransaction();
+
+            var user = User.builder()
+                    .username("mikhail@gmail.com")
+                    .firstname("Mikhail")
+                    .lastname("Belov")
+                    .birthDate(LocalDate.of(2000,11,18))
+                    .age(25)
+                    .build();
+
+            session.persist(user);
+            session.getTransaction().commit();
+        }
+    }
 }
